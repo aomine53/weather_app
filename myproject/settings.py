01064994530e5340130bb58d1e18c295adde45dd
Digvijay Shinde <digvijay5321shinde@gmail.com>
2021-05-12 15:46:46 +0530

    Fetch data from API

--- myproject/settings.py
+++ myproject/settings.py
@@ -59,7 +59,10 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            # os.path.join(BASE_DIR,'react-weather/build'),
+            os.path.join(BASE_DIR,'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,7 +127,18 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
--- weather_app/views.py
+++ weather_app/views.py
@@ -5,6 +5,16 @@ from django.http import JsonResponse, HttpResponse
 
 # Create your views here.
 def index(request):
-    url = "http://api.openweathermap.org/data/2.5/weather?q={city_nm}&appid=8554bee5ea12a558fe029889f3a5808c"
-    result = requests.get(url.format(city_nm = "mumbai"))
-    return JsonResponse(result.json(), safe = False)
+    url = "http://api.openweathermap.org/data/2.5/weather?q={city_nm}&appid=8554bee5ea12a558fe029889f3a5808c&units=metric"
+    result = requests.get(url.format(city_nm = "Mumbai"))
+    result = result.json()
+    # print(result.json())
+    args = {}
+    args['name'] = result['name']
+    args['desc'] = result['weather'][0]['description']
+    args['temp'] = result['main']['temp']
+    args['temp_max'] = result['main']['temp_max']
+    args['temp_min'] = result['main']['temp_min']
+    print(args)
+    # return JsonResponse(result, safe = False)
+    return render(request, 'index.html', args)
